8d403b99e25380ac359f158a282f9492630ef5b7
rafly-ananda <anandarafly15@gmail.com>
2023-08-03 16:20:09 +0700

    playwright

--- src/app/api/booking/callback/route.ts
+++ src/app/api/booking/callback/route.ts
@@ -1,8 +1,12 @@
-// import { Browser, BrowserContext, chromium, Page } from "playwright";
+import {
+  Browser,
+  BrowserContext,
+  chromium,
+  ChromiumBrowser,
+  Page,
+} from "playwright";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { NextResponse } from "next/server";
-import Chromium from "@sparticuz/chromium";
-import puppeteer from "puppeteer-core";
 
 export async function POST(
   request: Request,
@@ -35,14 +39,11 @@ export async function POST(
 
   // await s3Client.send(new PutObjectCommand(s3Params));
 
-  const browser = await puppeteer.launch({
-    args: Chromium.args,
-    defaultViewport: Chromium.defaultViewport,
-    headless: Chromium.headless,
-  });
-
-  const page = await browser.newPage();
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
   await page.goto("https://example.com");
+  const pageTitle = await page.title();
 
-  return NextResponse.json({ message: "ok", page: page.title });
+  return NextResponse.json({ message: "ok", page: pageTitle });
 }
